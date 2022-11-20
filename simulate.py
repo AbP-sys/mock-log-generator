78643cb372ea3aafcfa5953a19ee62694aeeb94c
AbP-sys <abhishekpagadala@gmail.com>
2022-11-20 15:00:23 +0530

    formatting date & time

--- simulate.py
+++ simulate.py
@@ -1,10 +1,14 @@
+import random
+import time
+
 def repeated_login():
     ip = "204.60.182.6"
-    date = "20/Nov/2020"
-    time = 1800
+    current_time = time.time()
     with open("log.txt",'w',encoding = 'utf-8') as f:
         for i in range(10):
-            f.write(ip+" - - ["+date+":"+str(time)+" +0000] \"PUT /en-us/account/login-gate/HTTP/1.0\" 200 14594\n")
-            time = (time+530) % 2400 #5 is input parameter
+            f.write(ip+" - - ["+time.strftime("%d/%b/%Y %H:%M:%S",
+             time.gmtime(current_time))+" +0000] \"PUT /en-us/account/login-gate/HTTP/1.0\" 200 "+
+             str(random.randrange(100, 25000, 1))+"\n")
+            current_time += 18000 #user input
 
 repeated_login()
