758a8a9ed3cc2f4561140a4943836725c810458a
AbP-sys <abhishekpagadala@gmail.com>
2022-11-21 22:52:44 +0530

    generate cpu usage (const)

--- simulate_cpu.py
+++ simulate_cpu.py
@@ -1,7 +1,7 @@
 import random
 import time
 
-def generate_random_data(lastEntry,current_time):
+def generate_spike_data(lastEntry,current_time):
     f = open("cpulogs.txt", "a")
     peak_chance = random.randint(0, 100)
     is_peak = False
@@ -26,8 +26,20 @@ def generate_random_data(lastEntry,current_time):
 
     return [cpu,mem]
 
+def generate_const_data(lastEntry,current_time):
+    f = open("cpulogs.txt", "a")
+    cpu = random.randint(lastEntry[0]-500,lastEntry[0]+500) 
+    mem = random.randint(lastEntry[1]-500, lastEntry[1]+500) 
+    f.write(time.strftime("%d %b %Y %H:%M:%S",
+             time.gmtime(current_time))+" CPU: "+str(cpu/100)+" Memory: "+str(mem/100)+"\n")
+    if cpu > 6000 or mem > 6000: #reset on increasing trend
+        return[3000,2500]
+    if cpu < 2000 or mem < 2000:
+        return[3000,2500]
+    return [cpu,mem]
+
 current_time = time.time()
 lastEntry = [3000,2500]
 for i in range (1000): 
-    lastEntry = generate_random_data(lastEntry,current_time)
+    lastEntry = generate_const_data(lastEntry,current_time)
     current_time += 300
