aac9970a96df4b1f1c4d663046ec7b9366624ec8
AbP-sys <abhishekpagadala@gmail.com>
2022-11-21 23:08:14 +0530

    simulate cpu usage (gradual increase)

--- simulate_cpu.py
+++ simulate_cpu.py
@@ -23,7 +23,6 @@ def generate_spike_data(lastEntry,current_time):
         return lastEntry
     if cpu > 6000 or mem > 6000: #reset on increasing trend
         return[3000,2500]
-
     return [cpu,mem]
 
 def generate_const_data(lastEntry,current_time):
@@ -38,8 +37,28 @@ def generate_const_data(lastEntry,current_time):
         return[3000,2500]
     return [cpu,mem]
 
+def generate_grad_data(lastEntry,current_time):
+    f = open("cpulogs.txt", "a")
+    dip_chance = random.randint(0, 100)
+    is_dip = False
+
+    if dip_chance < 20:  # 20% chance for generating a spike
+        cpu = random.randint(lastEntry[0]-500,lastEntry[0])
+        mem = random.randint(lastEntry[1]-500, lastEntry[1]) 
+        is_peak = True
+    else:
+        cpu = random.randint(lastEntry[0],lastEntry[0]+200) 
+        mem = random.randint(lastEntry[1], lastEntry[1]+200) 
+    if cpu >= 10000 or cpu <= 0:
+        cpu = lastEntry[0]
+    if mem >= 10000 or mem <= 0:
+        mem = lastEntry[1]
+    f.write(time.strftime("%d %b %Y %H:%M:%S",
+             time.gmtime(current_time))+" CPU: "+str(cpu/100)+" Memory: "+str(mem/100)+"\n")
+    return [cpu,mem]
+
 current_time = time.time()
 lastEntry = [3000,2500]
 for i in range (1000): 
-    lastEntry = generate_const_data(lastEntry,current_time)
+    lastEntry = generate_grad_data(lastEntry,current_time)
     current_time += 300
